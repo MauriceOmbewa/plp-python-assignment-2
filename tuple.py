3c67b48a182c1423dc62f78a7cd44df182ff7e5b
Maurice <ombewamaurice79@gmail.com>
2025-09-20 18:07:53 +0300

    Add tuple.py implementation

---
+++ tuple.py
@@ -0,0 +1,9 @@
+print("this is a tuple test")
+
+books = ('The alchemist', 'Think and grow rich', 'Rich dad poor dad', 'The art of war', 'The art of seduction')
+
+# for n in books:
+#     print(n)
+
+for i in range(len(books)):
+    print(books[i])
