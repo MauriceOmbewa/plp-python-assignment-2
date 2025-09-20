24b2568f0a8bb065b28671971801e45c5496ba4f
Maurice <ombewamaurice79@gmail.com>
2025-09-20 18:24:03 +0300

    Add dictionary.py implementation

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+print("simple dictionary program")
+print()
+
+temp_dictionary = {}
+
+name = input("What is your name? ")
+temp_dictionary["name"] = name
+
+age = input("What is your age? ")
+temp_dictionary["age"] = age
+
+gender = input("What is your gender? ")
+temp_dictionary["gender"] = gender
+
+print()
+print("the dictionary is => ", temp_dictionary)
